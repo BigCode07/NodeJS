d2f47456c45982e92cbae40932ccfc1d12d92248
Nicolas Saba <nicolassaba33@gmail.com>
2024-10-02 21:40:31 -0300

    Se finalizo la seccion 08 NOC

--- 05-Noc/src/presentation/server.ts
+++ 05-Noc/src/presentation/server.ts
@@ -1,19 +1,15 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./cron/cron-service";
 
-
 export class Server {
-
-    static Start(){
-        console.log("Server Started...");
-        CronService.createJob(
-            '*/5 * * * * *',
-            () =>{
-                new CheckService().execute('https://google.com');
-            }
-        );
-
-        
-    
-    }
+  static Start() {
+    console.log("Server Started...");
+    CronService.createJob("*/5 * * * * *", () => {
+      const url = "https://google.com";
+      new CheckService(
+        () => console.log(`${url} is ok`),
+        (error) => console.log(error)
+      ).execute("https://google.com");
+    });
+  }
 }
