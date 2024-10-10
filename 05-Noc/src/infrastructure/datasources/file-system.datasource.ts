f9e753d162bb81dd5215f2030dd933c98e64de36
Nicolas Saba <nicolassaba33@gmail.com>
2024-10-09 23:21:44 -0300

    Se vio hasta el curso 126 del curso

--- 05-Noc/src/infrastructure/datasources/file-system.datasource.ts
+++ 05-Noc/src/infrastructure/datasources/file-system.datasource.ts
@@ -39,7 +39,25 @@ export class FileSystemDataSource implements LogDatasource {
       fs.appendFileSync(this.mediumLogsPath, `${JSON.stringify(newLog)}\n`);
     }
   }
-  getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
-    throw new Error("Method not implemented.");
+
+  private getLogsFromFile = (path: string): LogEntity[] => {
+    const content = fs.readFileSync(path, "utf8");
+    const logs = content.split("\n").map((log) => LogEntity.fromJson(log));
+
+    return logs;
+  };
+
+  async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    switch (severityLevel) {
+      case LogSeverityLevel.low:
+        return this.getLogsFromFile(this.allLogsPath);
+      case LogSeverityLevel.medium:
+        return this.getLogsFromFile(this.mediumLogsPath);
+      case LogSeverityLevel.high:
+        return this.getLogsFromFile(this.highLogsPath);
+
+      default:
+        throw new Error(`Unknown severity level ${severityLevel}`);
+    }
   }
 }
